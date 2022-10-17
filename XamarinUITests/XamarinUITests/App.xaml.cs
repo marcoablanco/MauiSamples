905c21daebef2820c6ccbc544fc441461b8ec100
Marcos Antonio Blanco <marcoablanco@outlook.com>
2022-10-17 10:55:07 +0200

    Added empty project

---
+++ XamarinUITests/XamarinUITests/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XamarinUITests;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ XamarinUITests/XamarinUITests/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XamarinUITests;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
